b16696227fb9b91bbb258b86569c8d2ea45e288f
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-01 11:17:40 -0300

    Kickstart demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { IntlProvider, addLocaleData } from 'react-intl'
+import ptLocaleData from 'react-intl/locale-data/pt'
+import ptTranslations from '../../src/locales/pt'
+import mockRules from '../../src/rules/BRA'
+import ProfileContainer from '../../src/ProfileContainer'
+import 'vtex-tachyons'
 
-const App = () => {
-  return <h2>App works!</h2>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    addLocaleData(ptLocaleData)
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>ProfileForm demo:</h3>
+        <IntlProvider locale={'pt-BR'} messages={ptTranslations}>
+          <ProfileContainer rules={mockRules} />
+        </IntlProvider>
+      </div>
+    )
+  }
 }
 
 App.propTypes = {}
