7f3f7322cdb967463a36ff9cb8e4bab67520bb6a
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-01 18:24:14 -0300

    Add exports index

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Components
+export ProfileRules from './ProfileRules'
+export ProfileContainer from './ProfileContainer'
+
+// Helper Functions
+export { addValidation, removeValidation } from './validateProfile'
