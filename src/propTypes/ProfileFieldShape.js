a5c0f11ee11351b51e774df42c5d177308e4e0f0
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-01 15:28:34 -0300

    Adjust proptypes

--- src/propTypes/ProfileFieldShape.js
+++ src/propTypes/ProfileFieldShape.js
@@ -3,10 +3,8 @@ import PropTypes from 'prop-types'
 export default PropTypes.shape({
   /** The value for that field */
   value: PropTypes.any,
-  /** Whether field is valid or not */
-  valid: PropTypes.bool,
   /** i18n code for the error this value is presenting */
-  reason: PropTypes.string,
+  error: PropTypes.string,
   /** Whether the input should receive focus */
   focus: PropTypes.bool,
   /** Whether the input has already been touched by the user */
--- src/propTypes/RuleFieldShape.js
+++ src/propTypes/RuleFieldShape.js
@@ -4,7 +4,7 @@ export default PropTypes.shape({
   /** Name for both the field and the property */
   name: PropTypes.string.isRequired,
   /** Maximum length of the field */
-  maxLength: PropTypes.number.isRequired,
+  maxLength: PropTypes.number,
   /** i18n code for the label to be displayed over the field */
   label: PropTypes.string.isRequired,
   /** Whether the field is required or not */
