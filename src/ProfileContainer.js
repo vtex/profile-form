648375d929669c3068fd6e55abdce8b2db510c17
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-01 12:17:09 -0300

    Update component behaviors

--- src/ProfileContainer.js
+++ src/ProfileContainer.js
@@ -1,23 +1,65 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import ProfileInput from './ProfileInput'
+import ProfileField from './ProfileField'
+import { addValidation, removeValidation } from './validateProfile'
 
 class ProfileContainer extends Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      profile: null,
+    }
+  }
+
+  componentDidMount() {
+    const { profile } = this.props
+    this.setState({ profile: addValidation(profile) })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { profile: propProfile, onProfileChange } = this.props
+    const { profile: stateProfile } = this.state
+
+    if (prevState.profile !== stateProfile && onProfileChange) {
+      onProfileChange(stateProfile)
+    } else if (prevProps.profile !== propProfile) {
+      this.setState({ profile: addValidation(propProfile) })
+    }
+  }
+
+  handleFieldUpdate = field => {
+    this.setState(prevState => ({
+      profile: { ...prevState.profile, ...field },
+    }))
   }
 
   render() {
     const { rules } = this.props
+    const { profile } = this.state
+
+    if (!profile) return null
+
+    removeValidation(profile)
+
     return (
-      <div>{rules.fields.map(field => <ProfileInput field={field} />)}</div>
+      <div>
+        {rules.fields.map(field => (
+          <ProfileField
+            key={field.name}
+            field={field}
+            data={profile[field.name]}
+            onFieldUpdate={this.handleFieldUpdate}
+          />
+        ))}
+      </div>
     )
   }
 }
 
 ProfileContainer.propTypes = {
   rules: PropTypes.any.isRequired,
+  profile: PropTypes.any.isRequired,
+  onProfileChange: PropTypes.func,
 }
 
 export default ProfileContainer
--- src/ProfileInput.js
+++ src/ProfileInput.js
@@ -1,49 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { intlShape, injectIntl } from 'react-intl'
 import Input from '@vtex/styleguide/lib/Input'
 
-class ProfileInput extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  render() {
-    const { field, intl } = this.props
-
-    const data = {}
-
-    return (
-      <div
-        className={`vtex-profile-form__${field.name} ${
-          field.hidden ? 'dn' : ''
-        } pb7`}
-      >
-        <Input
-          label={intl.formatMessage({
-            id: `profile-form.field.${field.label}`,
-          })}
-          error={false}
-          errorMessage={
-            data.reason &&
-            intl.formatMessage({
-              id: `profile-form.error.${data.reason}`,
-            })
-          }
-          placeholder={
-            !field.required
-              ? this.props.intl.formatMessage({ id: 'profile-form.optional' })
-              : null
-          }
-        />
-      </div>
-    )
-  }
+const ProfileInput = ({ field, intl, data, onChange }) => {
+  return (
+    <div
+      className={`vtex-profile-form__${field.name} ${
+        field.hidden ? 'dn' : ''
+      } pb7`}
+    >
+      <Input
+        name={field.name}
+        label={intl.formatMessage({
+          id: `profile-form.field.${field.label}`,
+        })}
+        value={data.value || ''}
+        errorMessage={
+          data.reason &&
+          intl.formatMessage({
+            id: `profile-form.error.${data.reason}`,
+          })
+        }
+        placeholder={
+          !field.required
+            ? intl.formatMessage({ id: 'profile-form.optional' })
+            : null
+        }
+        onChange={onChange}
+      />
+    </div>
+  )
 }
 
 ProfileInput.propTypes = {
   field: PropTypes.any,
+  data: PropTypes.any,
+  onChange: PropTypes.func,
   intl: intlShape.isRequired,
 }
 
