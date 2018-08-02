2798399972a497eb9f3fda316015916339bdda61
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-02 12:11:22 -0300

    Add masking

--- src/ProfileField.js
+++ src/ProfileField.js
@@ -3,14 +3,25 @@ import PropTypes from 'prop-types'
 import RuleFieldShape from './propTypes/RuleFieldShape'
 import ProfileFieldShape from './propTypes/ProfileFieldShape'
 
+import msk from 'msk'
+
 class ProfileField extends Component {
   handleChange = e => {
     const { field, data, onFieldUpdate } = this.props
     const { value } = e.target
 
+    const maskedValue = this.applyMask(field, value)
     // validation will take place here
 
-    onFieldUpdate({ [field.name]: { ...data, value } })
+    onFieldUpdate({ [field.name]: { ...data, value: maskedValue } })
+  }
+
+  applyMask = (field, value) => {
+    if (field.mask) {
+      const mask = field.mask()
+      return msk.fit(value, mask)
+    }
+    return value
   }
 
   render() {
