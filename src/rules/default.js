34e30980675ff9a128bee72c7bdd42fdafb9251a
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-03 14:47:27 -0300

    Improve default rules

--- src/rules/default.js
+++ src/rules/default.js
@@ -1,4 +1,10 @@
+import moment from 'moment'
+import msk from 'msk'
+import Phone from '@vtex/phone'
+import brazil from '@vtex/phone/countries/BRA'
+
 export default {
+  country: 'BRA',
   personalFields: [
     {
       name: 'firstName',
@@ -13,16 +19,52 @@ export default {
       required: true,
     },
     {
+      name: 'document',
+      maxLength: 50,
+      label: 'document',
+      required: true,
+    },
+    {
       name: 'gender',
       maxLength: 30,
       label: 'gender',
     },
     {
-      name: 'document',
-      maxLength: 50,
-      label: 'cpf',
-      hidden: true,
+      name: 'birthDate',
+      maxLength: 30,
+      label: 'birthDate',
+    },
+    {
+      name: 'homePhone',
+      maxLength: 30,
+      label: 'homePhone',
     },
   ],
-  businessFields: [],
+  businessFields: [
+    {
+      name: 'corporateName',
+      maxLength: 100,
+      label: 'corporateName',
+    },
+    {
+      name: 'corporateDocument',
+      maxLength: 30,
+      label: 'corporateDocument',
+    },
+    {
+      name: 'businessPhone',
+      maxLength: 30,
+      label: 'businessPhone',
+    },
+    {
+      name: 'stateRegistration',
+      maxLength: 50,
+      label: 'stateRegistration',
+    },
+    {
+      name: 'tradeName',
+      maxLength: 100,
+      label: 'tradeName',
+    },
+  ],
 }
