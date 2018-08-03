08e10e7373dd9a2dc944d7daa0faa93de417838e
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-03 14:36:50 -0300

    Add validation for CPF and CNPJ

--- src/rules/pt-BR.js
+++ src/rules/pt-BR.js
@@ -23,6 +23,29 @@ export default {
       maxLength: 50,
       label: 'cpf',
       mask: value => msk.fit(value, '999.999.999-99'),
+      validate: value => {
+        const cleanValue = value.replace(/[^\d]/g, '')
+        if (cleanValue.length != 11) return false
+
+        const isRepeatedNum = '0123456789'
+          .split('')
+          .some(digit => digit.repeat(11) === cleanValue)
+        if (isRepeatedNum) return false
+
+        const firstReduce = cleanValue
+          .split('')
+          .slice(0, 9)
+          .reduce((acc, cur, index) => acc + parseInt(cur) * (10 - index), 0)
+        const firstDigit = ((firstReduce * 10) % 11) % 10
+        if (firstDigit != cleanValue.charAt(9)) return false
+
+        const secondReduce = cleanValue
+          .split('')
+          .slice(0, 10)
+          .reduce((acc, cur, index) => acc + parseInt(cur) * (11 - index), 0)
+        const secondDigit = ((secondReduce * 10) % 11) % 10
+        return secondDigit == cleanValue.charAt(10)
+      },
     },
     {
       name: 'gender',
@@ -57,11 +80,50 @@ export default {
       name: 'corporateDocument',
       maxLength: 30,
       label: 'cnpj',
+      mask: value => msk.fit(value, '99.999.999/9999-99'),
+      validate: value => {
+        const cleanValue = value.replace(/[^\d]/g, '')
+        if (cleanValue.length != 14) return false
+
+        const isRepeatedNum = '0123456789'
+          .split('')
+          .some(digit => digit.repeat(14) === cleanValue)
+        if (isRepeatedNum) return false
+
+        const firstWeights = '543298765432'.split('')
+        const firstReduce = cleanValue
+          .split('')
+          .slice(0, 12)
+          .reduce(
+            (acc, cur, index) =>
+              acc + parseInt(cur) * parseInt(firstWeights[index]),
+            0,
+          )
+        const firstDigit = firstReduce % 11 < 2 ? 0 : 11 - (firstReduce % 11)
+        if (firstDigit != cleanValue.charAt(12)) return false
+
+        const secondWeights = ['6', ...firstWeights]
+        const secondReduce = cleanValue
+          .split('')
+          .slice(0, 13)
+          .reduce(
+            (acc, cur, index) =>
+              acc + parseInt(cur) * parseInt(secondWeights[index]),
+            0,
+          )
+        const secondDigit = secondReduce % 11 < 2 ? 0 : 11 - (secondReduce % 11)
+        return secondDigit == cleanValue.charAt(13)
+      },
     },
     {
       name: 'businessPhone',
       maxLength: 30,
       label: 'businessPhone',
+      mask: value =>
+        value.length === 14
+          ? msk.fit(value, '(99) 9999-9999')
+          : msk.fit(value, '(99) 99999-9999'),
+      validate: value => Phone.validate(value, '55'),
     },
     {
       name: 'stateRegistration',
