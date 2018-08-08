37845cd89dc432f18ba31c810e07dafe312fc473
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-08 14:08:41 -0300

    Add ProfileSummary export link

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Components
 export { default as ProfileRules } from './ProfileRules'
 export { default as ProfileContainer } from './ProfileContainer'
+export { default as ProfileSummary } from './ProfileSummary'
 
 // Helper Functions
 export { addValidation, removeValidation } from './validateProfile'
