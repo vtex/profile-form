2ad7caedc427d06bc42f54afa6060fb61e21dc2e
Gustavo Silva <gustavo.faustino@vtex.com.br>
2018-08-16 10:36:28 -0300

    Add rules handling

--- scripts/build-io.sh
+++ scripts/build-io.sh
@@ -1,8 +1,9 @@
 cp -R entrypoints react
+mv react/rules/* react
+rm -rf react/rules
+cp package.json react
 cp -R src react/components
 mv react/components/locales/ react
 cd react/components/
-rm -rf __mocks__/ propTypes/ index.js *.test.js setupTests.js
-mv rules/* ../
-rm -rf rules/
+rm -rf __mocks__/ index.js *.test.js setupTests.js
 cd ../../
