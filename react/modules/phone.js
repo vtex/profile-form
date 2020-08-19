5a8e73cfb62efa4691d1e800973363ce4a57856c
biancameira <bianca.lopes@vtex.com.br>
2020-08-19 17:07:09 -0300

    Change translation, mexican field and phone mask

--- react/modules/phone.js
+++ react/modules/phone.js
@@ -1,27 +1,30 @@
 import Phone from '@vtex/phone'
 
 export function validatePhone(countryCode) {
-  return (value) => Phone.validate(value, countryCode)
+  return value => Phone.validate(value, countryCode)
 }
 
 export function formatPhone(countryCode) {
-  return (value) => {
-    const phone = value.indexOf('+') === 0
-      ? Phone.getPhoneInternational(value, countryCode)
-      : Phone.getPhoneNational(value, countryCode)
+  return value => {
+    const phoneDigits = value.replace(/[^\d]+/g, '')
+    const phone =
+      value.indexOf('+') === 0
+        ? Phone.getPhoneInternational(phoneDigits, countryCode)
+        : Phone.getPhoneNational(phoneDigits, countryCode)
 
-    if (!phone) return value
+    if (!phone) return value.replace(/[^\d]+/g, '')
 
     return Phone.format(phone, Phone.NATIONAL)
   }
 }
 
 export function cleanPhone(countryCode) {
-  return (value) => {
+  return value => {
     const numbers = value.replace(/[^\d]/g, '')
 
     try {
       const phone = Phone.getPhoneNational(numbers, countryCode)
+
       if (!phone) return value
 
       return Phone.format(phone, Phone.INTERNATIONAL)
