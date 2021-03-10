d656f6c73705b24ef3b441a29963ae9aa2d6773f
jordi-castellanos <39369951+jordi-castellanos@users.noreply.github.com>
2021-03-10 15:45:42 +0100

    Create BEL.js

---
+++ react/rules/BEL.js
@@ -0,0 +1,67 @@
+import france from '@vtex/phone/countries/BEL'
+
+import { getPhoneFields } from '../modules/phone'
+import initialize from './initializeCountryPhone'
+import { isPastDate } from '../utils/dateRules'
+
+const phoneCountryCode = initialize(france)
+
+export default {
+  country: 'BEL',
+  personalFields: [
+    {
+      name: 'firstName',
+      maxLength: 100,
+      label: 'firstName',
+      required: true,
+    },
+    {
+      name: 'lastName',
+      maxLength: 100,
+      label: 'lastName',
+      required: true,
+    },
+    {
+      name: 'email',
+      maxLength: 100,
+      label: 'email',
+      hidden: true,
+    },
+    {
+      name: 'homePhone',
+      maxLength: 30,
+      label: 'homePhone',
+      ...getPhoneFields(phoneCountryCode),
+    },
+    {
+      name: 'gender',
+      maxLength: 30,
+      label: 'gender',
+    },
+    {
+      name: 'birthDate',
+      maxLength: 30,
+      label: 'birthDate',
+      type: 'date',
+      validate: isPastDate,
+    },
+  ],
+  businessFields: [
+    {
+      name: 'corporateName',
+      maxLength: 100,
+      label: 'corporateName',
+    },
+    {
+      name: 'tradeName',
+      maxLength: 100,
+      label: 'tradeName',
+    },
+    {
+      name: 'businessPhone',
+      maxLength: 30,
+      label: 'businessPhone',
+      ...getPhoneFields(phoneCountryCode),
+    },
+  ],
+}
