3eca771ae986cd44516f67fe36547d3a1d88317f
GabrielBarross <gabriel.barros@vtex.com.br>
2026-07-15 14:59:31 -0300

    feat: add native phone validation rules for Ireland (IRL)

---
+++ react/rules/IRL.js
@@ -0,0 +1,67 @@
+import irl from '@vtex/phone/countries/IRL' // Used for initialization purposes, do not remove it!
+
+import { getPhoneFields } from '../modules/phone'
+import initialize from './initializeCountryPhone'
+import { isPastDate } from '../utils/dateRules'
+
+const phoneCountryCode = initialize(irl)
+
+export default {
+  country: 'IRL',
+  personalFields: [
+    {
+      name: 'firstName',
+      maxLength: 100,
+      label: 'firstName',
+      required: true,
+    },
+    {
+      name: 'lastName',
+      maxLength: 100,
+      label: 'lastName',
+      required: true,
+    },
+    {
+      name: 'email',
+      maxLength: 100,
+      label: 'email',
+      hidden: true,
+    },
+    {
+      name: 'homePhone',
+      maxLength: 30,
+      label: 'homePhone',
+      ...getPhoneFields(phoneCountryCode),
+    },
+    {
+      name: 'gender',
+      maxLength: 30,
+      label: 'gender',
+    },
+    {
+      name: 'birthDate',
+      maxLength: 30,
+      label: 'birthDate',
+      type: 'date',
+      validate: isPastDate,
+    },
+  ],
+  businessFields: [
+    {
+      name: 'corporateName',
+      maxLength: 100,
+      label: 'corporateName',
+    },
+    {
+      name: 'tradeName',
+      maxLength: 100,
+      label: 'tradeName',
+    },
+    {
+      name: 'businessPhone',
+      maxLength: 30,
+      label: 'businessPhone',
+      ...getPhoneFields(phoneCountryCode),
+    },
+  ],
+}
